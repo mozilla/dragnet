eb704472741446b68741ccb43827899bf6040901
Brandon Savage <brandon@brandonsavage.net>
2011-07-11 11:21:20 -0400

    Renaming directory to DLL for simplification. Correcting issues identified in code review.

---
+++ apps/dll/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+class Comment(models.Model):
+    '''Comments users have made on given DLL files'''
+    user_id = models.ForeignKey(User)
+    dll_id = models.ForeignKey('File')
+    date = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+    
+class File(models.Model):
+    '''The actual DLL file itself'''
+    STATUS_UNKNOWN = 'unknown'
+    STATUS_VALID = 'valid'
+    STATUS_MALWARE = 'malware'
+    STATUS_CHOICES = (
+        (STATUS_UNKNOWN, 'Unknown'),
+        (STATUS_VALID,     'Valid'),
+        (STATUS_MALWARE, 'Malware')
+    )
+    date_created = models.DateTimeField(default=datetime.datetime.now)
+    date_modified = models.DateTimeField(default=datetime.datetime.now, auto_now=True)
+    created_by = models.ForeignKey(Users)
+    file_name = models.CharField(max_length=200)
+    common_name = models.CharField(max_length=200, blank=True, null=True)
+    vendor = models.CharField(max_length=200, blank=True, null=True)
+    distributors = models.CharField(max_length = 200, blank=True, null=True)
+    md5_hash = models.CharField(max_length = 32, blank=True, null=True)
+    sha1_hash = models.CharField(max_length=40, blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    released = models.DateField(blank=True, null=True)
+    obsolete = models.BooleanField(default=False)
+    replaced_by = models.CharField(max_length=200, blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+    
+class FileHistory(models.Model):
+    '''A historical record of the DLL file and the changes made to it over time'''
+    dllid = models.ForeignKey(File)
+    userid = models.ForeignKey(Users)
+    date_changed = models.DateTimeField(auto_now=True)
+    field = models.CharField(max_length=40)
+    original_state = models.CharField(max_length=200)
+    changed_state = models.CharField(max_length=200)
